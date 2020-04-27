9ff447f2e89c49028e9479f905227c6dfdc14e87
Hans-Peter Schadler <hps@abyle.org>
2020-04-27 17:35:09 +0200

    discord: replace " with \"

--- platform/discord/messageconverter.go
+++ platform/discord/messageconverter.go
@@ -3,5 +3,7 @@ package discord
 import "strings"
 
 func convertMessageFromAbyleBotter(text string) string {
-	return strings.ReplaceAll(text, "\n", "\\n")
+	n := strings.ReplaceAll(text, "\n", "\\n")
+	n = strings.ReplaceAll(n, `"`, `\"`)
+	return n
 }
