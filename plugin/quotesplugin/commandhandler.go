bfa30b011f546c15eb7ce161783595e637d466b7
Hans-Peter Schadler <hps@abyle.org>
2020-05-19 17:05:25 +0200

    fix quoteadd message

--- plugin/quotesplugin/commandhandler.go
+++ plugin/quotesplugin/commandhandler.go
@@ -157,8 +157,7 @@ func (p *QuotesPlugin) onCommandQuoteAdd(post model.Post) {
 
 	num := p.storeQuote(quote)
 	if num > 0 {
-		// TODO: Message when adding Quote may actually report the quote number offset by one
-		p.returnMessage(post.ChannelID, fmt.Sprintf("Successfully added quote #%d", num))
+		p.returnMessage(post.ChannelID, fmt.Sprintf("Successfully added quote #%d", num-1))
 	} else {
 		p.returnMessage(post.ChannelID, "Error storing quote. Try again later!")
 	}
